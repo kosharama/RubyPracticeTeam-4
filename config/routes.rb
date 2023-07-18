57773cbb4826a250b3419dcc42383cc24a440d26
kpso4ka <sanyo20200@gmail.com>
2023-07-18 03:16:34 +0300

    work with Frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :doctors
   resources :meets
+  resources :users, only: [:new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
